6bf11efe360d95893e59b503b20d3e73309512bd
dxzmpk <1635338905@qq.com>
2021-01-18 22:36:03 +0800

    rename model

--- src/main/java/BostonRegressionModel.java
+++ src/main/java/BostonRegressionModel.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class IrisClassifyModel implements IModel{
+public class BostonRegressionModel implements IModel{
 
     List<Double> w;
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     
     public static void main(String[] args){
         IDataLoader dataLoader = new DadaLoader();
-        IModel model = new IrisClassifyModel();
+        IModel model = new BostonRegressionModel();
         Path path =  Paths.get("dataset");
         path = path.resolve("normalized_housing.csv");
         //dataLoader.loadData("D:\\archive\\test.csv");
