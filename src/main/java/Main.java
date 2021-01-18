708a1df025407df2bd729e2765a3a841062fd968
dxzmpk <1635338905@qq.com>
2021-01-18 22:32:25 +0800

    store dataset into project directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     
     public static void main(String[] args){
         IDataLoader dataLoader = new DadaLoader();
         IModel model = new IrisClassifyModel();
+        Path path =  Paths.get("dataset");
+        path = path.resolve("normalized_housing.csv");
         //dataLoader.loadData("D:\\archive\\test.csv");
-        dataLoader.loadData("D:\\archive\\normalized_housing.csv");
+        dataLoader.loadData(path.toAbsolutePath().toString());
         model.train(dataLoader.getTrain_data(), dataLoader.getTrain_labels());
     }
 }
